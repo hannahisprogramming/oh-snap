023cad1c2c7c5a1d4cbe5495d62b0328db2c152c
Hannah Bush <heb.rutgers@gmail.com>
2022-05-25 16:19:43 -0400

    nav created

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
       <h1 id="about">Who am i?</h1>
       <img src={coverImage} className="my-2" style={{width:"100%"}} alt="cover" />
     </section>
-  )
+  );
 }
 
 export default About;
