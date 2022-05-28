de8b81157ed6f399833cddaff8083d1fe3286d8d
Hannah Bush <heb.rutgers@gmail.com>
2022-05-28 01:11:38 -0400

    input returns added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,25 @@ import React from "react";
 
 function ContactForm() {
   //jsx
+  return (
+    <section>
+      <h1 id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type={email} name="email" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" />
+          <button type="submit">Submit</button>
+        </div>
+      </h1>
+    </section>
+  );
 }
 
 export default ContactForm;
